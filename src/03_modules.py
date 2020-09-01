6da0cf0f943c9fae3940621328c037c3a3edc579
Timothy Gilliam <timgill987@gmail.com>
2020-09-01 10:42:52 -0600

    made a second branch for pt and finished

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,12 +9,15 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
+
 # YOUR CODE HERE
+print("\n".join(sys.argv))
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print(sys.platform)
 # Print out the version of Python you're using:
+print(sys.version)
 # YOUR CODE HERE
 
 
@@ -22,10 +25,13 @@ import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
+print(os.getpid)
 # YOUR CODE HERE
 
 # Print the current working directory (cwd):
+print(os.getcwd)
 # YOUR CODE HERE
 
 # Print out your machine's login name
+print(os.getlogin)
 # YOUR CODE HERE
